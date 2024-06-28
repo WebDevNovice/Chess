eb9015b3fc97a96b90e7442040dfc09c15501602
WebDevNovice <jacobgbullock3@gmail.com>
2024-06-28 12:59:07 -0600

    Working Mainly on King Move List. Hoping once I figure out the King's moves all the others will be easier.

--- shared/src/main/java/chess/KingMoveCalc.java
+++ shared/src/main/java/chess/KingMoveCalc.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * So this needs to store and array of positions
  * that means that I'll need to be able to "visit" each step
@@ -11,10 +14,32 @@ public class KingMoveCalc {
 
     private final ChessPosition myPosition;
     private final ChessBoard board;
+    private final int[][] myArray = new int[8][8];
 
     public KingMoveCalc(ChessBoard board, ChessPosition myPosition) {
         this.board = board;
         this.myPosition = myPosition;
     }
 
+    public enum KingPieceMove {
+        UP(-1,0),
+        DOWN(1,0),
+        LEFT(0,-1),
+        RIGHT(0,1),
+        DIAONALUPLEFT(-1,-1),
+        DIAGONALUPRIGHT(-1,1),
+        DIAGONALDOWNLEFT(1,-1),
+        DIAGONALDOWNRIGHT(1,1);
+
+        private final int rowChange;
+        private final int colChange;
+
+        KingPieceMove(int rowChange, int colChange) {
+            this.rowChange = rowChange;
+            this.colChange = colChange;
+        }
+    }
+    public List<ChessPosition> getValidMoves() {
+        return new ArrayList<>();
+    }
 }
