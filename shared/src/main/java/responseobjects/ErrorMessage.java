ef390c075157cab34b8b7abe2faf4d52c7c610c9
WebDevNovice <jacobgbullock3@gmail.com>
2024-08-06 16:50:17 -0600

    I'm hoping

--- shared/src/main/java/responseobjects/ErrorMessage.java
+++ shared/src/main/java/responseobjects/ErrorMessage.java
@@ -1,8 +1,11 @@
-package server;
+package responseobjects;
 
 public class ErrorMessage {
     private String message;
     public ErrorMessage(String message) {
         this.message = message;
+    }
+    public String getMessage() {
+        return message;
     }
 }
